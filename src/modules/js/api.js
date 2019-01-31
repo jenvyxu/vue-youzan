d4447271ffaff1fe85b67498d38440768e4ee001
xzw <firetcandy@gmail.com>
2019-02-01 04:26:38 +0800

    添加子菜单

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -2,9 +2,9 @@
 let url={
   hotLists:'/index/hotLists',
   //http://mock.eolinker.com/A3s4BIEb3cd1095d5d9fd45a81ad657f2dbbf70bd145b4f?uri=/index/hotLists
-  banner:'/index/banner'
+  banner:'/index/banner',
   //http://mock.eolinker.com/A3s4BIEb3cd1095d5d9fd45a81ad657f2dbbf70bd145b4f?uri=/index/banner
-
+  topLists:'/category/topLists'
 }
 //开发环境和线上环境的切换
 //let host=
--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -1,2 +1,32 @@
 import 'css/common.css'
 import './category.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import Foot from 'components/Foot.vue'
+
+new Vue({
+  el:'#app',
+  data:{
+    topLists:null,
+    topIndex:0
+  },
+  methods:{
+    getTopList(){
+      axios.post(url.topLists).then(res=>{
+        this.topLists=res.data.lists
+      })
+    },
+    getSubList(id,index){
+      this.topIndex=index
+    }
+  },
+  created(){
+    this.getTopList()
+  },
+  components:{
+    Foot
+  }
+})
+
