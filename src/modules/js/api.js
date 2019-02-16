34afef4b711300a970a584b5f0c16339dcc70ae9
xzw <firetcandy@gmail.com>
2019-02-16 18:23:25 +0800

    完善搜索页面

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,12 +1,11 @@
 //与后台对接
 let url={
   hotLists:'/index/hotLists',
-  //http://mock.eolinker.com/A3s4BIEb3cd1095d5d9fd45a81ad657f2dbbf70bd145b4f?uri=/index/hotLists
   banner:'/index/banner',
-  //http://mock.eolinker.com/A3s4BIEb3cd1095d5d9fd45a81ad657f2dbbf70bd145b4f?uri=/index/banner
   topLists:'/category/topLists',
   subLists:'/category/subLists',
   rank:'/category/rank',
+  searchList:'/search/list'
 
 }
 //开发环境和线上环境的切换
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,1 +1,40 @@
 import './search.css'
+import 'css/common.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import qs from 'qs'
+import Velocity from 'velocity-animate'
+import mixin from 'js/mixin.js'
+
+let {keyword,id}=qs.parse(location.search.substr(1))
+
+new Vue({
+  el:'.container',
+  data:{
+    searchList:null,
+    show:false,
+  },
+  methods:{
+    getsearchList(){
+      axios.post(url.searchList,{keyword,id}).then(res=>{
+        this.searchList=res.data.lists
+      })
+    },
+    move() {
+      if(window.scrollY > 100) {
+        this.show = true
+      } else {
+        this.show = false
+      }
+    },
+    toTop(){
+      Velocity(document.body, "scroll", { duration: 1000 })
+    }
+  },
+  created(){
+    this.getsearchList()
+  },
+  mixins:[mixin]
+})
