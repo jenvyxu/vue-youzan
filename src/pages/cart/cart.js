5954051c28b8115af1e4bc0fa626061f24984705
jenvyxu <1272117264@qq.com>
2019-06-21 22:48:22 +0800

    添加商品编辑功能

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -9,64 +9,103 @@ import url from 'js/api.js'
 
 
 new Vue({
-    el:'.container',
-    data:{
-        lists:null
+    el: '.container',
+    data: {
+        lists: null,
+        total:0,
+        editingShop:null,
+        editingShopIndex:-1
     },
-    computed:{
-        allSelected:{
-            get(){
-                if(this.lists&&this.lists.length){
-                    return this.lists.every(shop=>{
+    computed: {
+        allSelected: {
+            get() {
+                if (this.lists && this.lists.length) {
+                    return this.lists.every(shop => {
                         return shop.checked
                     })
                 }
 
                 return false
             },
-            set(newVal){
-                this.lists.forEach(shop=>{
-                    shop.checked=newVal
-                    shop.goodsList.forEach(good=>{
-                        good.checked=newVal
+            set(newVal) {
+                this.lists.forEach(shop => {
+                    shop.checked = newVal
+                    shop.goodsList.forEach(good => {
+                        good.checked = newVal
                     })
                 })
 
             }
-        }
-    },
-    created(){
-        this.getList()
-    },
-    methods:{
-        getList(){
-            axios.post(url.cartLists).then(res=>{
-                let lists=res.data.cartList
-                lists.forEach(shop=>{
-                    shop.checked=true
+        },
+        selectLists(){
+            if(this.lists&&this.lists.length){
+                let arr=[]
+                let total=0
+                this.lists.forEach(shop=>{
                     shop.goodsList.forEach(good=>{
-                        good.checked=true
+                        if(good.checked){
+                            arr.push(good)
+                            total+=good.price*good.number
+                        }
                     })
                 })
-                this.lists=lists //处理完lists再渲染页面，否则不能实现响应式
+                this.total=total
+                return arr
+            }
+            return []
+        },
+        removeLists(){
+
+
+        }
+    },
+    created() {
+        this.getList()
+    },
+    methods: {
+        getList() {
+            axios.post(url.cartLists).then(res => {
+                let lists = res.data.cartList
+                lists.forEach(shop => {
+                    shop.checked = true
+                    shop.removeChecked=false
+                    shop.editing=false
+                    shop.editingMsg='编辑'
+                    shop.goodsList.forEach(good => {
+                        good.checked = true
+                        good.removeChecked=false
+                    })
+                })
+                this.lists = lists //处理完lists再渲染页面，否则不能实现响应式
             })
         },
-        selectGood(shop,good){
-            good.checked=!good.checked
-            shop.checked=shop.goodsList.every(good=>{
+        selectGood(shop, good) {
+            good.checked = !good.checked
+            shop.checked = shop.goodsList.every(good => {
                 return good.checked
             })
         },
-        selectShop(shop){
-            shop.checked=!shop.checked
-            shop.goodsList.forEach(good=>{
-                good.checked=shop.checked
+        selectShop(shop) {
+            shop.checked = !shop.checked
+            shop.goodsList.forEach(good => {
+                good.checked = shop.checked
             })
         },
-        selectAll(){
-            this.allSelected=!this.allSelected
-            console.log(this.allSelected)
+        selectAll() {
+            this.allSelected = !this.allSelected
+        },
+        edit(shop,shopIndex){
+            shop.editing=!shop.editing
+            shop.editingMsg=shop.editing?'完成':'编辑'
+            this.lists.forEach((item,i)=>{
+                if(shopIndex!==i){
+                    item.editing=false
+                    item.editingMsg=shop.editing?'':'编辑'
+                }
+            })
+            this.editingShop=shop.editing?shop:null
+            this.editingShopIndex=shop.editing?shopIndex:-1
         }
     },
-    mixins:[mixin]
+    mixins: [mixin]
 })
