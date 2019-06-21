28733d8bedce7915480184a0c0128b72adddc9fb
jenvyxu <1272117264@qq.com>
2019-06-21 16:41:16 +0800

    完善购物车勾选商品功能

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -7,21 +7,65 @@ import mixin from 'js/mixin.js'
 import axios from 'axios'
 import url from 'js/api.js'
 
+
 new Vue({
     el:'.container',
     data:{
         lists:null
     },
-    computed:{},
+    computed:{
+        allSelected:{
+            get(){
+                if(this.lists&&this.lists.length){
+                    return this.lists.every(shop=>{
+                        return shop.checked
+                    })
+                }
+
+                return false
+            },
+            set(newVal){
+                this.lists.forEach(shop=>{
+                    shop.checked=newVal
+                    shop.goodsList.forEach(good=>{
+                        good.checked=newVal
+                    })
+                })
+
+            }
+        }
+    },
     created(){
         this.getList()
     },
     methods:{
         getList(){
             axios.post(url.cartLists).then(res=>{
-                this.lists=res.data.cartList
-                console.log(this.lists)
+                let lists=res.data.cartList
+                lists.forEach(shop=>{
+                    shop.checked=true
+                    shop.goodsList.forEach(good=>{
+                        good.checked=true
+                    })
+                })
+                this.lists=lists //处理完lists再渲染页面，否则不能实现响应式
             })
+        },
+        selectGood(shop,good){
+            good.checked=!good.checked
+            shop.checked=shop.goodsList.every(good=>{
+                return good.checked
+            })
+        },
+        selectShop(shop){
+            shop.checked=!shop.checked
+            shop.goodsList.forEach(good=>{
+                good.checked=shop.checked
+            })
+        },
+        selectAll(){
+            this.allSelected=!this.allSelected
+            console.log(this.allSelected)
         }
     },
     mixins:[mixin]
